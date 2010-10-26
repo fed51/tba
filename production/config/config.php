9085171814772653b6b03589886a376b9cbbd1eb
systembomber <deadpad@googlemail.com>
2010-10-26 16:01:20 +0100

    Added more configuration options

--- production/config/config.php
+++ production/config/config.php
@@ -6,13 +6,13 @@
      * globally accessable and will not change.
      **/     
      /** DATABASE CONSTANTS **/
-     define('DATABASE_HOST', 'qdbphpprj.db.5509031.hostedresource.com');
+     //define('DATABASE_HOST', 'qdbphpprj.db.5509031.hostedresource.com');     define('DATABASE_HOST', 'localhost');
      define('DATABASE_USER', 'qdbphpprj');
      define('DATABASE_PASS', 'Phpprj1');
      define('DATABASE_DB', 'qdbphpprj');
          
      /** URL */
-     define('BASE_URL', 'http://qdb.cj-baker.co.uk/production/');   // The base url (with trailing slash)
+     //define('BASE_URL', 'http://qdb.cj-baker.co.uk/production/');   // The base url (with trailing slash)     define('BASE_URL', 'http://localhost/Test/production/');
      define('DEFAULT_CONTROLLER', 'home');
      
      define('FOLDER_CLASSES', 'classes');
@@ -21,5 +21,5 @@
      
      define('BOOTSTRAPPER', 'bootstrapper.php');   
      /** SITE SPECIFIC */
-     define('SITE_NAME', 'Quotes Database');
+     define('SITE_NAME', 'Quotes Database');          define('SLASH', '/');
 ?>
