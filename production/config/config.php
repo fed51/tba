2ea7c698dfe0a27b084d59ecf23dcd82983318ab
systembomber <deadpad@googlemail.com>
2010-10-26 11:42:39 +0100

    Sorted EOL Conversions

--- production/config/config.php
+++ production/config/config.php
@@ -1,30 +1,25 @@
 <?php
-
-    /**
-     * @name Config
-     * 
-     * This file contains all the configuration information that needs to be
-     * globally accessable and will not change.
-     **/
-     
-     /** DATABASE CONSTANTS **/
-     define('DATABASE_HOST', 'qdbphpprj.db.5509031.hostedresource.com');
-     define('DATABASE_USER', 'qdbphpprj');
-     define('DATABASE_PASS', 'Phpprj1');
-     define('DATABASE_DB', 'qdbphpprj');
-     
-     
-     /** URL */
-     define('BASE_URL', 'http://qdb.cj-baker.co.uk/production/');   // The base url (with trailing slash)
-     define('DEFAULT_CONTROLLER', 'home');
-     
-     define('FOLDER_CLASSES', 'classes');
-     define('FOLDER_CONTROLLERS', 'controllers');
-     define('FOLDER_LAYOUT', 'layout');
-     
-     define('BOOTSTRAPPER', 'bootstrapper.php');
-     
-     /** SITE SPECIFIC */
+    /**
+     * @name Config
+     * 
+     * This file contains all the configuration information that needs to be
+     * globally accessable and will not change.
+     **/     
+     /** DATABASE CONSTANTS **/
+     define('DATABASE_HOST', 'qdbphpprj.db.5509031.hostedresource.com');
+     define('DATABASE_USER', 'qdbphpprj');
+     define('DATABASE_PASS', 'Phpprj1');
+     define('DATABASE_DB', 'qdbphpprj');
+         
+     /** URL */
+     define('BASE_URL', 'http://qdb.cj-baker.co.uk/production/');   // The base url (with trailing slash)
+     define('DEFAULT_CONTROLLER', 'home');
+     
+     define('FOLDER_CLASSES', 'classes');
+     define('FOLDER_CONTROLLERS', 'controllers');
+     define('FOLDER_LAYOUT', 'layout');
+     
+     define('BOOTSTRAPPER', 'bootstrapper.php');   
+     /** SITE SPECIFIC */
      define('SITE_NAME', 'Quotes Database');
-
 ?>
--- production/controllers/home.php
+++ production/controllers/home.php
@@ -1,15 +1,12 @@
-<?php
-
-    class Home extends Controller {
-        
-        function __construct() {
-            parent::__construct();
-            echo "<br />(Home) Home::construct";   
-        }
-        
-        public function index() {
-            echo "<br />(Home) Home::index";
-            $this->load->view("bob");
-        }
-        
+<?php
+    class Home extends Controller {
+        
+        function __construct() {
+            parent::__construct();
+            echo "<br />(Home) Home::construct";   
+        }        
+        public function index() {
+            echo "<br />(Home) Home::index";
+            $this->load->view("bob");
+        }       
     }
